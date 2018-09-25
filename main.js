a791739654858f26d31895dd78ce53e2323d07e2
Josiah Hopkins <jhopkins@infoblox.com>
2018-09-25 10:39:38 -0700

    Has flooding and genetic seed weight

---
+++ main.js
@@ -0,0 +1,296 @@
+function getGameControl(game){
+    var toReturn = game.gameController;
+    toReturn.riverReach = 8;
+    return toReturn;
+}
+
+
+// GameBoard code below
+
+function randomInt(n) {
+    return Math.floor(Math.random() * n);
+}
+
+function rgb(r, g, b) {
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function Agent(game, x, y, agent) {
+    if (agent) {
+        var bit = randomInt(2)
+        this.seedWeight = agent.seedWeight + Math.pow(-1, bit) * Math.random() * 0.1;
+        if (this.seedWeight < 0) this.seedWeight = 0;
+        if (this.seedWeight > 1) this.seedWeight = 1;
+    }
+    else {
+        this.seedWeight = 0.5;
+    }
+
+    var val = Math.floor(256 * this.seedWeight);
+    this.isSeed = true;
+    this.color = rgb(val,val,val);
+    if( this.seedWeight < .3){
+        this.color = "Orange";
+    } else if (this.seedWeight > .7){
+        this.color = "Purple";
+    } else{
+        this.color = "Yellow";
+    }
+    this.maxAge = 50;
+    this.age = this.maxAge;
+
+    Entity.call(this, game, x, y);
+}
+
+Agent.prototype = new Entity();
+Agent.prototype.constructor = Agent;
+
+Agent.prototype.update = function () {
+    var SEEDDROP = 2;
+    var cell = this.game.board.board[this.x][this.y];
+    var dropDistance = Math.floor(this.seedWeight * 6) + 1;
+    var seedHardiness = dropDistance * dropDistance;
+
+    if(this.isSeed){
+        if(cell.water > 0 && (Math.random() > 0.02 * cell.population * cell.population)){
+            this.isSeed = false;
+            cell.population++;
+            cell.water -= 1;
+            //Log genetics
+            if(Math.random() < .0001){
+                console.log(this.seedWeight);
+            }
+        }
+        else if(Math.random() < seedHardiness){
+            this.dead = true;
+        }
+    } else{
+        cell.water -= 1;
+        // did I die?
+        if (Math.random() < 0.02 * cell.population * cell.population) {
+            this.dead = true;
+            cell.population -= 1;
+            for(var i = 0; i < SEEDDROP; i++){
+                var newX = Math.floor((this.x + randomInt(dropDistance) - (dropDistance / 2) + this.game.board.dimension) % this.game.board.dimension);
+                var newY = Math.floor((this.y + randomInt(dropDistance) - (dropDistance / 2) + this.game.board.dimension) % this.game.board.dimension);
+                if(newX < 100 && newY < 100 && newX >= 0 && newY >= 0){
+                    var newCell = this.game.board.board[newX][newY];
+                    if(newCell.seedCount * newCell.seedCount * .02 < Math.random() && !newCell.isRiver){
+                        var agent = new Agent(this.game, newX, newY, this);
+                        this.game.babies.push(agent); 
+                    }
+                }
+            }
+        } 
+        //Dying of thirst prevents reproduction
+        else if (cell.water < 1){
+            this.dead = true; 
+            cell.population -= 1;
+        }
+    }
+}
+
+function Cell(game,x,y) {
+    this.x = x;
+    this.y = y;
+    this.game = game;
+    this.isRiver = false;
+    this.water = 0;
+    this.waterDistance=-1;
+    this.seedCount = 0;
+
+    this.population = 0;
+
+    this.color = "Grey";
+}
+
+Cell.prototype = new Entity();
+Cell.prototype.constructor = Cell;
+
+Cell.prototype.update = function () {
+    MAX_WATER = 20; //TO_DO
+    /*
+    This should replenish or deplete resources as necessary on each turn update. Resource determining factors will all be under gameControll object
+    */
+   if(this.waterDistance > 0 && this.water < MAX_WATER){
+        if(this.game.gameController.floodSeason){
+            //Replenish during flood season. Should double water and have water table extend to a further range as determined by a cells 
+            //riverdistance
+            var WATER_DISTANCE = 11//this.game.gameController.riverReach;//TODO
+            if(this.waterDistance < WATER_DISTANCE){
+                this.water += (WATER_DISTANCE - this.waterDistance);
+                this.water = Math.min(this.water, MAX_WATER);
+            }
+        } else {
+            //Standard replenishment. Note that the rate of flooding is determined in the gameengine update function.
+            var WATER_DISTANCE = 11//this.game.gameController.riverReach; //TODO
+            WATER_DISTANCE = Math.floor(WATER_DISTANCE / 2);
+            if(this.waterDistance < WATER_DISTANCE){
+                this.water += (WATER_DISTANCE - this.waterDistance);
+                this.water = Math.min(this.water, MAX_WATER);
+            }
+        }
+    }
+}
+
+
+function Automata(game) {
+    var SPLITCHANCE = .01;
+    this.dimension = 100;
+    this.populationSize = 100;
+    this.agents = [];
+    game.babies = [];
+
+    // create board
+    this.board = [];
+    for (var i = 0; i < this.dimension; i++) {
+        this.board.push([]);
+        for (var j = 0; j < this.dimension; j++) {
+            this.board[i].push(new Cell(game,i,j));
+        }
+    }
+    var allRiverTiles = [];
+    var currentRiverTilesInRow= [];
+    var nextRiverTiles = [];
+    currentRiverTilesInRow.push(Math.round(this.dimension / 3), Math.round((2 * this.dimension) / 3))
+    for( var i = 0; i < this.dimension; i++){
+        for(var placement = 0; placement < currentRiverTilesInRow.length; placement++){
+            //Turn this tile into river
+            this.board[currentRiverTilesInRow[placement]][i].isRiver = true;
+            this.board[currentRiverTilesInRow[placement]][i].color = "Blue";
+            allRiverTiles.push([currentRiverTilesInRow[placement], i]);
+            for(var addingWater = 1; addingWater < 11; addingWater++){
+                var left = this.board[currentRiverTilesInRow[placement] - addingWater][i]
+                var right = this.board[currentRiverTilesInRow[placement] + addingWater][i]
+                if(left && !left.isRiver){
+                    if(addingWater < 11){
+                        left.water += 11 - addingWater;
+                    } 
+                    left.waterDistance = Math.min(addingWater, addingWater);
+                }
+                if(right && !right.isRiver){
+                    if(addingWater < 11){
+                        right.water += 11 - addingWater;
+                    } 
+
+                    right.waterDistance = addingWater;
+                }
+            }
+
+            if(Math.random() < SPLITCHANCE){
+                //split once left and once right
+                nextRiverTiles.push(currentRiverTilesInRow[placement] + 1, currentRiverTilesInRow[placement] - 1);
+            } else{
+                //ChooseRightLeftOrMiddle
+                var riverDirection = Math.random();
+                if(riverDirection < .33){
+                    nextRiverTiles.push(currentRiverTilesInRow[placement] + 1);
+                } else if(riverDirection < .67){
+                    nextRiverTiles.push(currentRiverTilesInRow[placement] - 1);
+                } else {
+                    nextRiverTiles.push(currentRiverTilesInRow[placement]);
+                }
+            }
+        }
+        currentRiverTilesInRow = nextRiverTiles;
+        nextRiverTiles = [];
+    }
+
+    // add agents
+    
+    while (this.agents.length < this.populationSize) {
+        var x = randomInt(this.dimension);
+        var y = randomInt(this.dimension);
+
+        var agent = new Agent(game, x, y);
+        this.agents.push(agent);
+        this.board[x][y].population += 1;
+    }
+    
+
+    Entity.call(this, game, 0, 0);
+};
+
+Automata.prototype = new Entity();
+Automata.prototype.constructor = Automata;
+
+Automata.prototype.update = function () {
+    for (var i = 0; i < this.agents.length; i++) {
+        this.agents[i].update();
+    }
+
+    for (var i = this.agents.length - 1; i >= 0; i--) {
+        if (this.agents[i].dead) {
+            this.agents.splice(i, 1);
+        }
+    }
+    for (var i = this.game.babies.length - 1; i >= 0; i--){
+        this.agents.push(this.game.babies.pop());
+    }
+    this.game.babies = [];
+
+    //while (this.agents.length < this.populationSize) {
+    //    var parent = this.agents[randomInt(this.agents.length)];
+    //    var agent = new Agent(this.game, parent.x, parent.y, parent);
+    //    this.agents.push(agent);
+    //}
+
+    for (var i = 0; i < this.dimension; i++) {
+        for (var j = 0; j < this.dimension; j++) {
+            this.board[i][j].update();
+        }
+    }
+};
+
+Automata.prototype.draw = function (ctx) {
+    var size = 8;
+    for (var i = 0; i < this.dimension; i++) {
+        for (var j = 0; j < this.dimension; j++) {
+            var cell = this.board[i][j];
+            
+            color = rgb(Math.min(cell.population * 80),Math.min(cell.population * 80), 0);
+            if(this.seedWeight < .25){
+                color = "Orange";
+            } else if( this.seedWeight > .75){
+                color = "Green";
+            }
+            if(cell.population === 0){
+                color = cell.color;
+            }
+            ctx.fillStyle = color;
+            ctx.fillRect(i * size, j * size, size, size);
+        }
+    }
+
+    /*
+    for (var i = 0; i < this.agents.length; i++) {
+        ctx.fillStyle = this.agents[i].color;
+        ctx.beginPath();
+        ctx.arc((this.agents[i].x * size) + (size / 2), (this.agents[i].y * size) + (size / 2), (size / 2), 0, 2 * Math.PI, false);
+        ctx.fill();
+    }
+    */
+};
+
+
+// the "main" code begins here
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
+ASSET_MANAGER.queueDownload("./img/black.png");
+ASSET_MANAGER.queueDownload("./img/white.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    console.log("starting up da sheild");
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
+
+
+    var gameEngine = new GameEngine();
+    var automata = new Automata(gameEngine);
+    gameEngine.addEntity(automata);
+    gameEngine.board = automata;
+    gameEngine.init(ctx);
+    gameEngine.start();
+});
