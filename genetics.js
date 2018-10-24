8e29c2aa6bbd08be0edd60227b5fd6eb4e6adac0
josiahhopkins <hopkinsjosiah@gmail.com>
2018-10-23 18:36:47 -0700

    Created the genetics class to try and generalize how I will make genetic properties and all and hopefully make the code cleaner

--- genetics.js
+++ genetics.js
@@ -1,9 +1,26 @@
 function Genetics(){
-    this.valueMap = {};
+    this.valueMap = [];
+    this.addProperty = function(name, value, the_function){
+        this.valueMap.name = GeneticProperty(value, the_function);
+    }
 }
 
-Genetics.prototype.constructor = Genetics;
+function GeneticProperty(value, the_function){
+    this.value = value;
+    this.myFunction = the_function;
+}
 
-function GeneticProperty(value){
-    this.value = value
+function defaultFunction(value){
+    var bit = randomInt(2);
+    toReturn = value + Math.pow(-1, bit) * Math.random() * 0.1;
+    makeSureValueBetweenZeroAndOne(toReturn);
+}
+
+function makeSureValueBetweenZeroAndOne(value){
+    if (value < 0){
+        return 0;
+    } else if (value > 1){ 
+        return 1;
+    }
+    return value;
 }
